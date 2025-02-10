6d684a61aedd9d9655bf254ae8ace93ef2ab1fa3
szabozsokam <szabozsokam@gmail.com>
2025-02-10 17:56:16 +0100

    update drivermanager method

--- lib/drivermanager.py
+++ lib/drivermanager.py
@@ -1,16 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service as EdgeService
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 def install_driver(browser):
     if browser == "edge":
-        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+        driver = webdriver.Edge()
         return driver
     elif browser == "chrome":
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver = webdriver.Chrome()
         return driver
     else:
         raise Exception("Invalid browser type!")
